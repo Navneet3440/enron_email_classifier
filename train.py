6ee1ce278614614186c7eb96b3de91970e13536c
Navneet3440 <navneetmec3440@gmail.com>
2022-06-02 22:37:21 +0530

    change workers of dataloader to 2

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ def run():
     )
 
     train_data_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=config.TRAIN_BATCH_SIZE, num_workers=8, shuffle=True
+        train_dataset, batch_size=config.TRAIN_BATCH_SIZE, num_workers=2, shuffle=True
     )
 
     valid_dataset = dataset.BERTDataset(
