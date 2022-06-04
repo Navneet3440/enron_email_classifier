e84494722ee465edb6775d353882efbf699d9ae8
Navneet3440 <navneetmec3440@gmail.com>
2022-06-04 16:53:13 +0530

    changes with forward removed

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import transformers
 
 DEVICE = "cuda"
-MAX_LEN = 256
+MAX_LEN = 512
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 8
 EPOCHS = 50
@@ -10,12 +10,12 @@ RETRAIN = False
 NUMBER_OF_CLASS = 6
 RETRAIN_MODEL_LOC = "./inputs/bert-base-multilingual-cased_128_ds2_ba_lr1e-05.bin"
 TRAINING_MODE = "ba"
-BERT_PATH = "bert-large-uncased"
+BERT_PATH = "bert-base-uncased"
 DATASET_FILE_VERSION_COUNT = 1
 MODEL_PATH = f"./inputs/{BERT_PATH}_{MAX_LEN}_ds{DATASET_FILE_VERSION_COUNT}_{TRAINING_MODE}_lr{str(LEARNING_RATE)}_"
 MODEL_PATH_2 = f"./inputs/dump/{BERT_PATH}_{MAX_LEN}_ds{DATASET_FILE_VERSION_COUNT}_{TRAINING_MODE}_lr{str(LEARNING_RATE)}_"
 TRAINING_FILE = (
-    f"./final_data.csv"
+    f"./final_data_forward_removed.csv"
 )
 if "cased" in BERT_PATH.split("-"):
     TOKENIZER = transformers.BertTokenizer.from_pretrained(
